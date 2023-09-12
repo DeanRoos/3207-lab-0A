233b2c22dd69c3fd62b8ae35e0ed09165b86814e
DeanRoos <tug91912@temple.edu>
2023-09-12 15:18:21 -0400

    Setting up command line arguments

--- tuls.c
+++ tuls.c
@@ -3,13 +3,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main (){
+int main (int argc, char *argv[] ){
 
-	DIR *dp;
-	struct dirent *d;
-	dp = opendir(".");
-	while ((d = readdir(dp)) != 0){
+	if (argc > 2){
+		printf("%s", "Incorrect arguments. Exiting program.");
+		exit(1);
+	} else if (argc == 1){
+		DIR *dp;
+		struct dirent *d;
+		dp = opendir(".");
+		while ((d = readdir(dp)) != 0){
 		printf("%s\n", d->d_name);
+		}
+	} else {
+
+
+		printf("%s", "2 arguments");
+
+
+
 	}
 	return 0;
 }
